2ecf13d5bbe8a64d011e96e0055165a49795c43f
Tiago Martins <vargas.tiago@hotmail.com>
2025-02-10 13:36:18 -0300

    feat: Created initial configs with vite, add tailwind and shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
