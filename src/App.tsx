e007712b4c56406d0cc7aeeebe2a7c1995a8c495
Tiago Martins <vargas.tiago@hotmail.com>
2025-02-12 18:03:06 -0300

    feat: Config React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
