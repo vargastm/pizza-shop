018791713a055498576023e13e98472fb58ef5f8
Tiago Martins <vargas.tiago@hotmail.com>
2025-02-12 15:54:06 -0300

    feat: Config React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
