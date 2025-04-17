63092c0d6e029340a7eab3d62f8d247a5b11a036
Tiago Martins <vargas.tiago@hotmail.com>
2025-04-16 21:48:22 -0300

    feat: Add pagination to orders list

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,9 @@
 import { api } from '@/lib/axios'
 
+export interface GetOrdersQuerry {
+  pageIndex?: number | null
+}
+
 export interface GetOrdersResponse {
   orders: {
     orderId: string
@@ -15,10 +19,10 @@ export interface GetOrdersResponse {
   }
 }
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrdersQuerry) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
